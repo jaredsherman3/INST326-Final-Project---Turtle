a3798b0c2604ddf28733ac278fb1bb4f3c8b1e24
Jared Sherman <jaredsherman@wireless-10-104-89-51.umd.edu>
2023-04-03 23:53:25 -0400

    added docstring for init method

--- final_project_turtle.py
+++ final_project_turtle.py
@@ -2,5 +2,10 @@
 
 class Song():
     def __init__(self, title):
+        """ Initalizes a new instance of Song class 
+        
+        Args: 
+            title (str): title of a song
+        """
         self.title = title
         
