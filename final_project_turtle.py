6a57db9cdfa78d5cd1cfb8477d50684f5900c8a2
ChiaEE <chiamakaebulu@gmail.com>
2023-04-04 18:13:00 -0400

    Added Playlist Method

--- final_project_turtle.py
+++ final_project_turtle.py
@@ -9,7 +9,19 @@ class Song():
         """
         self.title = title
         
-
+def playlist (track, artist):
+        ''' Creates a dictionary of different songs
+            Args:
+                track (str): song written by an artist
+                artist(str): the artist who wrote the song
+            Returns:
+                play (dict): a dictionary of songs with the song as the key 
+                and the artist as the value'''
+        i = 0
+        play = {}
+        while i < 16:
+            play[track]= artist
+            i+=1
 
 def main(): 
     """ Finds a song based on the users personality 
