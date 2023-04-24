a62048b77e517dc0107835630ea13e68536bcba1
Jared Sherman <jsherm3#terpmail.umd.edu>
2023-04-24 10:05:25 -0400

    removed my class

--- final_project_turtle.py
+++ final_project_turtle.py
@@ -1,47 +1,6 @@
 """ Song suggestions for a user based on personality """
 
-traits = {
-    "creative": 4,
-    "adventurous": 3,
-    "outgoing": 2,
-    "emotional": 1,
-    "organized": -1,
-    "serious": -2,
-    "introverted": -3,
-    "guarded": -4
-}
 
-class PersonalityTest():
-    """A class representing a personality test.
-
-    Attributes:
-        traits (dict): A dictionary of traits and their corresponding scores.
-    """
-    def __init__(self, traits):
-        """Initializes instance of PersonalityTest class"""
-        self.traits = traits
-        
-    def user_score(self):
-        """Prompt user to rate themselves on each trait"""
-        user_scores = {}
-        for trait, score in self.traits.items():
-            rating = int(input(f"On a scale of 1 to 5, how {trait} are you? "))
-            user_scores[trait] = rating * score
-        return user_scores
-    
-    def personality_type(self, user_scores):
-        """Take the users score and return personality type"""
-        total_score = sum(user_scores.values())
-        if total_score > 10:
-            return "openness"
-        elif total_score > 0:
-            return "creative"
-        elif total_score > -10:
-            return "reserved"
-        else:
-            return "guarded"
-        
-        #OPTION 2 FOR PERSONALITY TEST BASED ON THE BIG FIVE
 class BigFiveTest:
     """ This test is formatted a bit diff from above. I think it is a bit easier to work
     with but we can discuss details. Personally am a big fan of the big five test. Anyway
