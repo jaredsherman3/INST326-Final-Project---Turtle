9993374d2078047392bd582a04fcd9de17133557
ChiaEE <chiamakaebulu@gmail.com>
2023-04-30 11:06:24 -0400

    Edited Playlist and high_trait_song methods

--- final_project_turtle.py
+++ final_project_turtle.py
@@ -144,37 +144,53 @@ class Song():
             title (str): title of a song
         """
         self.title = title
+
+    def high_trait_song(self,songdict):
+        ''' should give the User a specific song reccomendation best on highest trait
+        
+        Args: 
+        songdict (dict): derived from a JSON file that holds 5 dictioanries
+        with three keys 'Low, 'Medium', 'High' and each key has a value that is a list of 
+        different songs.Each dictionarybkey is meant to correspond to the traits in BigFiveTest - Chiamaka 
+        
+        Returns:
+        Reccomended song to user based on highest trait'''
+        highest = highest_score(self.trait_scores)
+        for key in songdict.keys():
+            if highest == key:
+                self.title = random.choice(songdict[key]['High'])
+
+        return f'A song that best describes you is: {self.title}' 
+    #Line above can be removed---> function can be used in main function
         
     def playlist (self,songdict):
         """Creates a dictionary of different songs
             Args:
-                songdict (dict): derived from JSON file that holds multiple dictionaries 
-                with songs that represent different time periods/genres and are
-                meant to correspond to the traits in BigFiveTest 
-                -- can edit this to fit other persoanlity test if necessary
-                 and I'm still working on the JSON file -> Chiamaka
+                songdict (dict): derived from a JSON file that holds 5 dictioanries
+                with three keys 'Low, 'Medium', 'High' and each key has a value that is a list of 
+                different songs.Each dictionarybkey is meant to correspond to the traits in BigFiveTest - Chiamaka
             Returns:
-                play (dict): a dictionary of songs with the artist as the key 
-                and the song as the value"""
-        # Will take the max value of the the highest trait and return a dictionary 
-        # (maybe a list?) of songs for the user 
+                play (list): A list of songs with the name and artist in the format ['song' by Artist Name]"""
+        # Will take the max value of the the highest trait and return a list of songs for the user 
         self.play = []
         User = BigFiveTest()
-        for key, value in User.trait_scores.items():
-            if key[value] == 2 or 3 or 4:
-                if key in songdict.keys():
-                    for i in range (2):
-                        self.play.append(random.choice(songdict[key]['Low']))
+        for trait, score in User.trait_scores.items():
+            if  2  >= score  <= 4:
+                if trait in songdict.keys():
+                    for i in range (3):
+                        self.play.append(random.choice(songdict[trait]['Low']))
 
-            if key[value] == 5 or 6 or 7:
-                -
+            elif  5 >= score  <= 7:
+                if trait in songdict.keys():
+                    for i in range (3):
+                        self.play.append(random.choice(songdict[trait]['Medium']))
 
-    def high_trait_song():
-        ''' should give the User a specific song reccomendation'''
-        highest = highest_score(User.trait_scores)
-        for key in songdict.keys():
-            if highest == key:
-                -
+            elif  8  >= score  <= 10:
+                if trait in songdict.keys():
+                    for i in range (3):
+                        self.play.append(random.choice(songdict[trait]['High']))
+            return self.play
+
 
                     
 def main(user):
